1ea2e89621e1964dd27ab2f6a0540f9cd7005925
sweta-singh28 <swetasinghgs28@gmail.com>
2024-01-28 21:40:55 +0530

    sweta

---
+++ Sum of two numbers.js
@@ -0,0 +1,13 @@
+//Write a function that takes two numbers as a parameters and return their sum
+function SumOfTwoNumbers (num1, num2){
+    return num1 + num2;
+}
+let result = SumOfTwoNumbers(5, 6);
+console.log(result);
+
+
+
+//let a = 5;
+// let b = 5;
+// let sum = a + b;
+// console.log(sum);
