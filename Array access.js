b140cd349bd8aa4d3b941ee32a7a4a2d1bbe99c0
sweta-singh28 <swetasinghgs28@gmail.com>
2024-04-30 17:41:25 +0530

    sweta

--- Array access.js
+++ Array access.js
@@ -1,4 +1,4 @@
 //Access the third element of an array called myArray
- let myArray = ["sweta", "Alex", "Joy", "Groot", "Gungun", "Lucy"];
+ let myArray = ["sweta", "Alex", "Joy", "Groot", "Gungun", "Lucy","ab"];
  let thirdElement = myArray[2];
  console.log(thirdElement);
