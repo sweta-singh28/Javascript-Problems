0b4ae17d23354fd9da39889ec52f406243d651ed
sweta-singh28 <swetasinghgs28@gmail.com>
2024-04-30 17:42:01 +0530

    heh

--- string concatination.js
+++ string concatination.js
@@ -1,5 +1,5 @@
 //Combine two string, "Hello" and "World" to create phrase "Hello World"
 var str = "Hello";
-var str1 = "World";
+var str1 = "World.";
 var result = str +  " "  + str1;
 console.log(result);
